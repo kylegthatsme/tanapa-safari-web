ff22468d3e2b1cf4ddcc4ceff947543f085ae9a4
Michael Vaughan <jmvaugh@clemson.edu>
2014-04-28 11:57:04 -0400

    Added edit_safari.php

---
+++ web/edit_safari.php
@@ -0,0 +1,40 @@
+<?php
+    
+    require('config/config.php');
+    $safari = array();
+    $stmt = $db_conn->prepare("SELECT s.id, s.name, s.description, s.tile_media_id, s.header_media_id, s.footer_media_id, tm.url tile_media_url, hm.url header_media_url, fm.url footer_media_url FROM SAFARI s LEFT JOIN MEDIA tm ON tm.id = s.tile_media_id LEFT JOIN MEDIA hm ON hm.id = s.header_media_id LEFT JOIN MEDIA fm ON fm.id = s.footer_media_id WHERE s.id = ?");
+    $stmt->bind_param('i', $_GET['id']);
+    $stmt->execute();
+    $stmt->bind_result($safari_id, $safari_name, $safari_description, $tile_media_id, $header_media_id, $footer_media_id, $tile_media_url, $header_media_url, $footer_media_url);
+    $stmt->fetch();
+    $safari["id"] = $safari_id;
+    $safari["name"] = $safari_name;
+    $safari["description"] = $safari_description;
+    $safari["tile_media_id"] = $tile_media_id;
+    $safari["header_media_id"] = $header_media_id;
+    $safari["footer_media_id"] = $footer_media_id;
+    $safari["tile_media_url"] = $tile_media_url;
+    $safari["header_media_url"] = $header_media_url;
+    $safari["footer_media_url"] = $footer_media_url;
+    $stmt->close();
+
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="page-header">
+            <h1>TANAPA Safari <small>Edit Safari</small></h1>
+        </div>
+        <form>
+            <?php
+                echo "<input type=\"text\" id=\"name\" placeholder=\"Name\" value=\"" . $safari["name"] . "\"/>";
+            ?>
+        </form>
+    </body>
+</html
