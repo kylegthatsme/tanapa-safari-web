01e07c6f95d31fb9d75c247d1174ce5b1fe724dc
Michael Vaughan <jmvaugh@clemson.edu>
2014-04-28 12:03:09 -0400

    Added edit_safari.php

--- web/edit_safari.php
+++ web/edit_safari.php
@@ -23,6 +23,9 @@
 <html>
     <head>
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+        
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 
         <!-- Latest compiled and minified JavaScript -->
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
